4dcdecff6e5e6f3637084d0aca5324e1683d9b98
Owen Park <96025896+owenjpark@users.noreply.github.com>
2024-10-17 01:57:50 -0700

    op - added homescreen ui

--- App.js
+++ App.js
@@ -2,13 +2,15 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './src/screens/HomeScreen';
 import CreateHousholdScreen from './src/screens/CreateHouseholdScreen';
+import LoginScreen from './src/screens/LoginScreen'
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen options={{ headerShown: false}} name="Login" component={LoginScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Create Household" component={CreateHousholdScreen} />
       </Stack.Navigator>
